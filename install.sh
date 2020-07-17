fdd2e65691c8dc09b0842c813ece6668c9a2fe2c
David Litwin <david@cinely.com>
2020-07-16 21:33:28 -0700

    upd

--- install.sh
+++ install.sh
@@ -1,31 +1,29 @@
 
-# Check if we have vim version 8.1+ installed
-VIM_PATH=$(which vim)
-VIM_VERSION=$(vim --version |grep '8.[123]')
-echo Vim path is: $VIM_PATH
-echo Vim version is: $VIM_VERSION
-if [ -z "$VIM_PATH" ] || [ -z "$VIM_VERSION" ]
-then
-    # Get OS
-    source /etc/os-release
-    if [ $NAME = 'Ubuntu' ]
-    then
-        echo 1/3. updating vim...
-        sudo add-apt-repository ppa:jonathonf/vim
-        sudo apt update
-        sudo apt install vim
-        apt-get install ctags # needs java if we want to do this
-        cd ~
-        echo 2/3. downloading vim from git...
-        git clone git@github.com:david542542/.vim.git
-        echo 3/3. installing plugins...
-        vim +PlugInstall +qall
-    else
-        echo 'Not ubuntu -- install manually'
-        exit
+# # Check if we have vim version 8.1+ installed
+# VIM_PATH=$(which vim)
+# VIM_VERSION=$(vim --version |grep '8.[123]')
+# echo Vim path is: $VIM_PATH
+# echo Vim version is: $VIM_VERSION
+
+# Get OS
+source /etc/os-release
+if [ $NAME = 'Ubuntu' ]; then
+    echo 1/3. updating vim...
+    sudo add-apt-repository ppa:jonathonf/vim
+    sudo apt update
+    sudo apt install vim
+    apt-get install ctags # needs java if we want to do this
+    echo 2/3. downloading vim from git...
+    cd ~
+    if [ -d ~/.vim ]; then 
+        rm -rf ~/.vim
     fi
+    git clone git@github.com:david542542/.vim.git
+    echo 3/3. installing plugins...
+    vim +PlugInstall +qall
 else
-    echo Vim already installed
+    echo 'Not ubuntu -- install manually'
+    exit
 fi
 
 if [ -z "$(grep 'VIM,TMUX' ~/.bash_profile)" ]
